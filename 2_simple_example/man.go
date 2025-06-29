c0d31793bbe6f440c60cd36cf889b0abe9c59aa3
3shaan <3shaan.dev@proton.me>
2025-06-30 00:49:06 +0600

    golang learn start, till array

---
+++ 2_simple_example/man.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Int
+	fmt.Println(2)
+	fmt.Println(2 + 3)
+
+	// Float
+	fmt.Println(2.5)
+
+	// String
+	fmt.Println("Hello, World!")
+
+	// Boolean
+	fmt.Println(true)
+	fmt.Println(false)
+
+	// Complex
+	fmt.Println(1 + 2i)
+
+}
